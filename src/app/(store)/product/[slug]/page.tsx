723e103b6a9737c55d3b013708d9e8c2832df056
lacy <lacienemelo@Air-de-Laciene.local>
2023-12-11 10:42:47 -0300

    metadatas

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,11 +1,44 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
+import { Metadata } from "next";
 import Image from "next/image";
 
-export default function ProductPage() {
+interface ProductsProps {
+    params: {
+        slug: string
+    }
+}
+
+// memoização -> deduplicar
+
+async function getProduct(slug: string): Promise<Product[]> {
+    const response = await api(`/products/${slug}`, {
+        next: {
+            revalidate: 60 * 60, // 1 hour
+        }
+    })
+
+    const product = await response.json()
+
+    return product
+}
+
+export async function generateMetadata({ params }: ProductsProps): Promise<Metadata> {
+    const [product] = await getProduct(params.slug)
+
+    return {
+        title: product.title
+    }
+}
+
+export default async function ProductPage({ params }: ProductsProps) {
+    const [product] = await getProduct(params.slug)
+
     return (
         <div className="relative grid max-h-[860px] grid-cols-3">
             <div className="col-span-2 overflow-hidden">
                 <Image
-                    src="/moletom-never-stop-learning.png"
+                    src={product.image}
                     width={1000}
                     height={1000}
                     quality={100}
@@ -15,14 +48,30 @@ export default function ProductPage() {
 
             <div className="flex flex-col justify-center px-12">
                 <h1 className="text-3xl font-bold leading-tight">
-                    Moletom Never Stop Learning
+                    {product.title}
                 </h1>
 
-                <p className="mt-2 text-s leading-relaxed text-zinc-400">Moletom fabricado com 88% de algodão e 12% de poliéster.</p>
+                <p className="mt-2 text-sm leading-relaxed text-zinc-400">{product.description}</p>
 
                 <div className="mt-8 flex items-center gap-3">
-                    <span className="inline-block bg-violet-500 py-2.5 px-5 rounded-full font-semibold">R$129</span>
-                    <span className="text-sm text-zinc-400">Em 12x s/ juros de R$10,75</span>
+                    <span className="inline-block bg-violet-500 py-2.5 px-5 rounded-full font-semibold">
+                        {
+                            product.price.toLocaleString('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL',
+                                minimumFractionDigits: 0,
+                                maximumFractionDigits: 0
+                            })
+                        }
+                    </span>
+                    <span className="text-sm text-zinc-400">
+                        {
+                            (product.price / 12).toLocaleString('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL'
+                            })
+                        }
+                    </span>
                 </div>
 
                 <div className="mt-8 space-y-4">
